2c1961275002cd1c6945edd69412e52bafbf5788
Spriha Anand <sprihaanand@gmail.com>
2023-05-24 17:48:00 +0530

    j

--- src/components/Js/Navbar.js
+++ src/components/Js/Navbar.js
@@ -15,11 +15,11 @@ function Navbar() {
         </div>
 
         <div class="nav-links">
-          <a href="/PyCray/#/apidocs">API Docs</a>
+          <a href="/apidocs">API Docs</a>
           <a href="/">About us</a>
-          <a href="/PyCray/#/faq">FAQ</a>
-          <a href="/PyCray/#/tos">Terms of Service</a>
-          <a href="/PyCray/#/getstarted">
+          <a href="/faq">FAQ</a>
+          <a href="/tos">Terms of Service</a>
+          <a href="/getstarted">
             <button className="NavButton">Get Started for Free</button>
           </a>
         </div>
